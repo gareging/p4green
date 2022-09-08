4c241ae6a91d0fbe19849e4ab52727c2c9fb7b8e
gareging <mr.garegin.grigoryan@gmail.com>
2022-09-08 17:25:32 +0000

    exprmnt

--- experiments/traffic_consolidation_client.sh
+++ experiments/traffic_consolidation_client.sh
@@ -1,54 +1,76 @@
-# THRESHOLD1: 1000 Bytes/s
-# THRESHOLD2: 10000 Bytes/s
+# THRESHOLD1: 10000 Bytes/s
+# THRESHOLD2: 40000 Bytes/s
 
 # 1-5 low
-iperf -c 10.0.1.5 -t 40 -b 1000 -l 10 &
-iperf -c 10.0.1.5 -t 40 -b 1000 -l 10 &
-iperf -c 10.0.2.5 -t 40 -b 1000 -l 10 &
-iperf -c 10.0.2.5 -t 40 -b 1000 -l 10 &
-iperf -c 10.0.3.5 -t 40 -b 1000 -l 10 &
-iperf -c 10.0.3.5 -t 40 -b 1000 -l 10 &
+iperf -c 10.0.1.5 -t 40 -b 900 -l 10 &
+iperf -c 10.0.1.5 -t 40 -b 900 -l 10 &
+iperf -c 10.0.2.5 -t 40 -b 900 -l 10 &
+iperf -c 10.0.2.5 -t 40 -b 900 -l 10 &
+iperf -c 10.0.3.5 -t 40 -b 900 -l 10 &
+iperf -c 10.0.3.5 -t 40 -b 900 -l 10 &
+iperf -c 10.0.4.5 -t 40 -b 900 -l 10 &
+iperf -c 10.0.4.5 -t 40 -b 900 -l 10 &
 sleep 35
 # 5-9 growing
-iperf -c 10.0.4.5 -t 45 -b 7000 -l 10 &
+iperf -c 10.0.1.5 -t 45 -b 2500 -l 10 &
+iperf -c 10.0.1.5 -t 45 -b 2500 -l 10 &
 sleep 5
-iperf -c 10.0.4.5 -t 40 -b 7000 -l 10 &
+iperf -c 10.0.2.5 -t 40 -b 2500 -l 10 &
+iperf -c 10.0.2.5 -t 40 -b 2500 -l 10 &
 sleep 5
-iperf -c 10.0.1.5 -t 35 -b 7000 -l 10 &
+iperf -c 10.0.3.5 -t 35 -b 2500 -l 10 &
+iperf -c 10.0.3.5 -t 35 -b 2500 -l 10 &
 sleep 5
-iperf -c 10.0.1.5 -t 30 -b 7000 -l 10 &
+iperf -c 10.0.4.5 -t 30 -b 2500 -l 10 &
+iperf -c 10.0.4.5 -t 30 -b 2500 -l 10 &
 sleep 5
-iperf -c 10.0.2.5 -t 25 -b 7000 -l 10 &
+iperf -c 10.0.1.5 -t 25 -b 2500 -l 10 &
+iperf -c 10.0.1.5 -t 25 -b 2500 -l 10 &
 sleep 5
-iperf -c 10.0.2.5 -t 20 -b 7000 -l 10 &
+iperf -c 10.0.2.5 -t 20 -b 2500 -l 10 &
+iperf -c 10.0.2.5 -t 20 -b 2500 -l 10 &
 sleep 5
-iperf -c 10.0.3.5 -t 15 -b 7000 -l 10 &
+iperf -c 10.0.3.5 -t 15 -b 2500 -l 10 &
+iperf -c 10.0.3.5 -t 15 -b 2500 -l 10 &
 sleep 5
-iperf -c 10.0.3.5 -t 10 -b 7000 -l 10 &
+iperf -c 10.0.4.5 -t 10 -b 7500 -l 10 &
+iperf -c 10.0.4.5 -t 10 -b 7500 -l 10 &
 sleep 5
-iperf -c 10.0.4.5 -t 5 -b 7000 -l 10 &
+iperf -c 10.0.1.5 -t 5 -b 7500 -l 10 &
+iperf -c 10.0.1.5 -t 5 -b 7500 -l 10 &
 # 9-20 peak
-iperf -c 10.0.4.5 -t 100 -b 70000 -l 10 &
+iperf -c 10.0.2.5 -t 100 -b 7500 -l 10 &
+iperf -c 10.0.2.5 -t 100 -b 7500 -l 10 &
 sleep 10
-iperf -c 10.0.4.5 -t 90 -b 70000 -l 10 &
+iperf -c 10.0.3.5 -t 80 -b 7500 -l 10 &
+iperf -c 10.0.3.5 -t 80 -b 7500 -l 10 &
 sleep 10
-iperf -c 10.0.1.5 -t 80 -b 70000 -l 10 &
+iperf -c 10.0.4.5 -t 70 -b 7500 -l 10 &
+iperf -c 10.0.4.5 -t 70 -b 7500 -l 10 &
 sleep 10
-iperf -c 10.0.1.5 -t 70 -b 70000 -l 10 &
+iperf -c 10.0.1.5 -t 60 -b 7500 -l 10 &
+iperf -c 10.0.1.5 -t 60 -b 7500 -l 10 &
 sleep 10
-iperf -c 10.0.2.5 -t 60 -b 70000 -l 10 &
+iperf -c 10.0.2.5 -t 50 -b 7500 -l 10 &
+iperf -c 10.0.2.5 -t 50 -b 7500 -l 10 &
 sleep 10
-iperf -c 10.0.2.5 -t 50 -b 70000 -l 10 &
+iperf -c 10.0.3.5 -t 40 -b 7500 -l 10 &
+iperf -c 10.0.3.5 -t 40 -b 7500 -l 10 &
 sleep 10
-iperf -c 10.0.3.5 -t 40 -b 70000 -l 10 &
+iperf -c 10.0.4.5 -t 30 -b 7500 -l 10 &
+iperf -c 10.0.4.5 -t 30 -b 7500 -l 10 &
 sleep 10
-iperf -c 10.0.3.5 -t 30 -b 70000 -l 10 &
+iperf -c 10.0.1.5 -t 20 -b 7500 -l 10 &
+iperf -c 10.0.1.5 -t 20 -b 7500 -l 10 &
 sleep 10
-iperf -c 10.0.4.5 -t 20 -b 70000 -l 10 &
+iperf -c 10.0.2.5 -t 10 -b 7500 -l 10 &
+iperf -c 10.0.2.5 -t 10 -b 7500 -l 10 &
 sleep 10
-iperf -c 10.0.4.5 -t 10 -b 70000 -l 10 &
+iperf -c 10.0.3.5 -t 10 -b 7500 -l 10 &
+iperf -c 10.0.3.5 -t 10 -b 7500 -l 10 &
 sleep 10
-iperf -c 10.0.4.5 -t 10 -b 30000 -l 10 &
+iperf -c 10.0.4.5 -t 10 -b 2500 -l 10 &
+iperf -c 10.0.4.5 -t 10 -b 2500 -l 10 &
 sleep 10
 # 20-24 lowering
 # 1-5 low
@@ -58,5 +80,8 @@ iperf -c 10.0.2.5 -t 40 -b 1000 -l 10 &
 iperf -c 10.0.2.5 -t 40 -b 1000 -l 10 &
 iperf -c 10.0.3.5 -t 40 -b 1000 -l 10 &
 iperf -c 10.0.3.5 -t 40 -b 1000 -l 10 
+iperf -c 10.0.4.5 -t 40 -b 1000 -l 10 &
+iperf -c 10.0.4.5 -t 40 -b 1000 -l 10 
+sleep 40
 echo 'DONE'
 
